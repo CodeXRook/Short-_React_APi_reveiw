ebb03fcc34e6f6b13156994f7cf61a63d4778aa6
CodeXRook <lacekitfly@gmail.com>
2019-03-02 23:51:19 -0500

    render and  export done

--- src/container/home.js
+++ src/container/home.js
@@ -1,3 +1,13 @@
 import React from 'react'; //Routes from app will connect here
 
-class Home extends React.Component {}
+// HOME CLASS REACT COMPONENT
+class Home extends React.Component {
+
+    render() {
+        return(
+            <h1>Home Page</h1>
+        )
+    }
+}
+
+export default Home;
