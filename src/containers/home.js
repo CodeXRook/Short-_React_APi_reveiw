622ae169ffc02e602f145364821af949caa03a9f
CodeXRook <lacekitfly@gmail.com>
2019-03-03 01:51:15 -0500

    axios imported for api

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,9 +1,19 @@
 import React from 'react'; //Routes from app will connect here
+import axios from 'axios'; //AXIOS IS NEEDED TO MAKE API CALLS
 
 // HOME PAGE NEEDS AN API CALL TO SHOW LIST OF YOUTUBE VIDEOS
 // HOME CLASS IS STATFUL REACT COMPONENT
 class Home extends React.Component {
 
+    //PARTICULAR SEARCH TYPE
+    
+    state ={
+        searchTerm: 'skateboarding'
+    }
+
+    //HOW DO WE GET THE SEARCH? AXIOS. IMPORT AXIOS.
+
+    
     render() {
         return(
             <h1>Home Page</h1>
