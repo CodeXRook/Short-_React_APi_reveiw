7504339a4c25418d1b21bc3777ef8c4962fb5144
CodeXRook <lacekitfly@gmail.com>
2019-03-03 01:47:12 -0500

    home needs api to get youtube list

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,5 +1,6 @@
 import React from 'react'; //Routes from app will connect here
 
+// HOME PAGE NEEDS AN API CALL TO SHOW LIST OF YOUTUBE VIDEOS
 // HOME CLASS IS STATFUL REACT COMPONENT
 class Home extends React.Component {
 
