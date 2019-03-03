6fdadd69963e72c3034330e2abcb38693c99cefc
CodeXRook <lacekitfly@gmail.com>
2019-03-03 03:46:36 -0500

    new snippet

--- src/containers/home.js
+++ src/containers/home.js
@@ -53,8 +53,9 @@ class Home extends React.Component {
         const showVideoList= this.state.videos.map((e,i) => {
             return(
                 <li key={i}>
-                <Link to={'/video/'+e.id.videoID}>
+                <Link to={'/video/'+e.id.videoID}> //
                 < img src ={e.snippet.thumbnails.medium.url} alt=' />
+                  <h5>{e.snippet.tittle}</5>
                 </Link>
                 </li>
             )
