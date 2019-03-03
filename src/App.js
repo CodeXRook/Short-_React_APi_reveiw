d1e821e04b1042c183ce765f733f1678f92ab336
CodeXRook <lacekitfly@gmail.com>
2019-03-02 23:23:48 -0500

    new comments on ask

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import React, { Component } from 'react';// KEEP THIS LINE
  import {HashRouter, Link, Route} from 'react-router-dom'; //INTERGRATE ROUTER INTO APPLICATION
 
-
+//WHERE GOING TO MAKE TWO PAGERS ROUTES BELOW
+//-> PAGE 1 LIST OF MUSIC VIDEOS
+// -> PAGE 2 SHOWING THE YOUTUBE VIDEO
 
 class App extends Component {
   render() {
